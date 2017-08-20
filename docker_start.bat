c9c9310fe3f7a19eb2cc684a6f050349fc99da84
MeXos5 <mexanikus5@gmail.com>
2017-08-21 01:33:35 +0300

    Черновая версия Докера со всеми необходимыми файлами

--- docker_start.bat
+++ docker_start.bat
@@ -1,3 +1,4 @@
 docker-machine env --shell cmd default
+@FOR /f "tokens=*" %i IN ('docker-machine env --shell cmd default') DO @%i
 docker-machine env --shell=powershell | Invoke-Expression
 docker version
